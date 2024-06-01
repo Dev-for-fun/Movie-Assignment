f45ea171ebe806eee33828b5d5473ea9324c2833
unknown <gajendra.rathore.dev07@gmail.com>
2024-06-01 15:50:54 +0530

    cors issue solved

--- server/index.py
+++ server/index.py
@@ -12,7 +12,7 @@ load_dotenv()
 # Allow CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://ar-genie-assignment.netlify.app/"],  # Allows all origins
+    allow_origins=["*"],  # Allows all origins
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
